f8f071608fd1cd8a151df30ee82cdc78f27e83e2
倉園あずさ <kuraenazusa@kuraenazusanoMacBook-Air.local>
2021-08-14 02:26:40 +0900

    8/4_メールリンク問題発生5

--- src/store/index.js
+++ src/store/index.js
@@ -48,7 +48,7 @@ const actions = {
           console.log(username)
           //メール認証
           const actionCodeSettings = {
-            url: "http://" + location.host + "/signin",
+            url: "http://" + location.host + "/home",
             handleCodeInApp: true
           }
           firebase.auth().languageCode = "ja"
